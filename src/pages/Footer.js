ba7ea5ffe6e26879d9032d17feb8b35ee04a383e
swati-coder-boot <swatirkudalli@gmail.com>
2021-01-27 02:44:57 -0800

    sample template

---
+++ src/pages/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <section class="footer">
+                         <p>Copyright &copy; 2021 Company Name: GreetLabs Pvt.Ltd/Design: Swati
+                             (Full Stack Python Developer)</p>
+                </section>
+            </div>
+            
+            
+        )
+                                
+    }
+}
+
+export default Footer;
